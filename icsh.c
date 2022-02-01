6ee8a9c07d2f9771620a3a17d55da96c5b790b7e
Manit Gandhi <yourgitemail@example.com>
2022-02-01 14:54:09 +0700

    Reformat Code (Complete Milestone 1)

--- icsh.c
+++ icsh.c
@@ -17,21 +17,20 @@ void echo();
 
 void systemExit();
 
-void systemExit(){
+void systemExit() { //For exit
     int num = atoi(args[1]);
     int converted = WEXITSTATUS(num);
-    if(num>255){
+    if (num > 255) {
         printf("Bye\n");
         exit(converted);
-    }
-    else{
+    } else {
         printf("Bye\n");
         exit(num);
     }
 
 }
 
-void echo() {
+void echo() { //For echo command
     int length = sizeof(args) / sizeof(args[0]);
     for (int i = 1; i < length; i++) {
         if (args[i] == NULL) break;
@@ -41,7 +40,7 @@ void echo() {
 }
 
 void getCommand() {
-    memset(cmd,0,sizeof(cmd));
+    memset(cmd, 0, sizeof(cmd)); //deleting the command that was stored in cmd before
     printf("icsh $\t");
     fgets(cmd, 100, stdin);
     int len = strlen(cmd);
@@ -51,27 +50,23 @@ void getCommand() {
 void startShell() {
     while (1) {
         getCommand();
-        if (strcmp("!!", cmd)){
-            if(strstr(cmd,"echo")){
-                memset(prev,0,sizeof(prev));
+        if (strcmp("!!", cmd)) {
+            if (strstr(cmd, "echo")) {
+                memset(prev, 0, sizeof(prev));
                 memcpy(prev, cmd, strlen(cmd));
             }
         }
         splitCmd();
         if (!strcmp("", cmd)) {
             continue;
-        }
-        else if (!strcmp("exit", args[0])) {
+        } else if (!strcmp("exit", args[0])) {
             systemExit();
             break;
-        }
-        else if (!strcmp("echo", args[0])) {
+        } else if (!strcmp("echo", args[0])) {
             echo(args);
-        }
-        else if (!strcmp("!!", args[0])) {
+        } else if (!strcmp("!!", args[0])) {
             printf("%s\n", prev);
-        }
-        else{
+        } else {
             printf("bad command\n");
         }
     }
