3dc044c1001b3b67fbb4eb409e6b5445aa688f2d
Manit Gandhi <yourgitemail@example.com>
2022-02-04 20:54:21 +0700

    Reformat Code and Finish Milestone 3

--- icsh.c
+++ icsh.c
@@ -145,13 +145,13 @@ void startShell() {
             } else {
                 //If it is an external command, fork a new process
                 int id = fork();
-                if(id==0){ // Execute the external program
-                    if(execvp(args[0],args)==-1){ //If the execution fails, it is a bad command
+                if (id == 0) { // Execute the external program
+                    if (execvp(args[0], args) == -1) { //If the execution fails, it is a bad command
                         printf("bad command\n");
                     }
                     exit(0);
                 }
-                if(id != 0){
+                if (id != 0) {
                     wait(NULL);
                 }
             }
